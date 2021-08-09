28d6d993a5308f548430cc044e075ce4d7ad6cbf
ravipandhi <rpandhi2018@my.fit.edu>
2021-08-09 15:03:08 -0400

    action config2

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,10 @@ plugins {
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.5.21"
     kotlin("plugin.spring") version "1.5.21"
-    `maven-publish`
 }
 
 publishing {
+
     repositories {
         maven {
             name = "GitHubPackages"
@@ -54,5 +54,5 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
-fun publishing(configure: () -> Unit) {
+fun publishing(function: () -> Unit) {
 }
