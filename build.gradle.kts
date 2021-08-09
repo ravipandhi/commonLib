f7327bead186e78dc56985e66e0cb05a2d874e92
ravipandhi <rpandhi2018@my.fit.edu>
2021-08-09 15:09:10 -0400

    action config3

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.5.21"
     kotlin("plugin.spring") version "1.5.21"
+    id("maven-publish")
 }
 
 publishing {
@@ -54,5 +55,5 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
-fun publishing(function: () -> Unit) {
+fun publishing(configure: () -> Unit) {
 }
